e3ccf55ceba88bc0baaf01545bdfb613bd32ccc6
iammoq <mozytiger6@gmail.com>
2023-06-23 13:54:35 +0000

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
- # Creates a file in tmp
+# Creates a file in tmp
 
-file { '/tmp/school':
-  ensure  => file,
+file { 'school':
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
